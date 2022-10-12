89e01b866a9aa37c3072a999413a7d94eaf9140c
Gerson Calienes <devgerson@gmail.com>
2022-10-12 19:00:02 -0400

    Update server.js
    actual cause of error

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const app = express();
 const api = require('./routes/index.js');
 const PORT = process.env.PORT || 3001;
 
-app.use('/api', api);
 app.use(express.urlencoded({ extended: true}))
-app.use(express.json());
+app.use(express.json()); //this middleware will parse out JSON coming in, so has to go before the api routes
+
 app.use(express.static('public'));
+
+app.use('/api', api);
 
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
